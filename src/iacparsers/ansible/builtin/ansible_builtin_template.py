1f42fb3bc8b1c757dbba636d220bd6273494221c
Mirek Zurek <zurek.miroslaw@gmail.com>
2024-07-07 18:21:16 +0200

    Removed old files

--- src/iacparsers/ansible/builtin/ansible_builtin_template.py
+++
@@ -1,7 +0,0 @@
-class AnsibleBuiltinTemplate:
-    """
-    Description of Ansible Builtin Template Module:
-    https://docs.ansible.com/ansible/latest/collections/ansible/builtin/template_module.html
-    """
-
-    MODULENAME = "ansible.builtin.template"
