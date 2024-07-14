507dc9975b56ded4ebb444089213b9d4e4544897
Mirek Zurek <zurek.miroslaw@gmail.com>
2024-07-14 19:39:15 +0200

    pinkhat-19 Added policy for aws_db_instance to validate publicly_accessible

--- tests/terraform_aws/test_aws_db_instance.py
+++ tests/terraform_aws/test_aws_db_instance.py
@@ -10,12 +10,31 @@ issues = [
         "module": "aws_db_instance",
         "graph_name": "aws_db_instance.vulnerable_default",
         "description": "Not all patches and security fixes are automatically implemented in a database. "
-                       "The required work must be implemented during the maintenance window. "
-                       "https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/"
-                       "USER_UpgradeDBInstance.Maintenance.html#OS_Updates\n",
-        "remediation": 'Provide maintenance window based on your needs\n'
-                       'resource "aws_db_instance" "db_instance_name" '
-                       '{\n  ...\n  maintenance_window          = "Mon:00:00-Mon:03:00"\n}\n',
+        "The required work must be implemented during the maintenance window. "
+        "https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/"
+        "USER_UpgradeDBInstance.Maintenance.html#OS_Updates\n",
+        "remediation": "Provide maintenance window based on your needs\n"
+        'resource "aws_db_instance" "db_instance_name" '
+        '{\n  ...\n  maintenance_window          = "Mon:00:00-Mon:03:00"\n}\n',
+        "issue": None,
+        "line_of_code": 13,
+    },
+    {
+        "file_path": "tests/terraform_aws/test_files/aws_db_instance.tf",
+        "category": "terraform",
+        "rule_name": "Publicly accessible database",
+        "module": "aws_db_instance",
+        "graph_name": "aws_db_instance.vulnerable_default",
+        "description": "Publicly accessible database might lead to data leakage through:\n\n"
+        "  - security vulnerability in the database\n"
+        "  - default user name and password\n"
+        "  - weak passwords\n"
+        "  - and more\n\n"
+        "Database should be hidden and not accessible via internet.\n"
+        "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_instance#publicly_accessible\n"
+        "publicly_accessible - default value is false\n",
+        "remediation": "Disable publicly_accessible in aws_db_instance\n"
+        'resource "aws_db_instance" "db_instance_name" {\n  ...\n  publicly_accessible = false\n}\n',
         "issue": None,
         "line_of_code": 13,
     },
@@ -26,14 +45,14 @@ issues = [
         "module": "aws_db_instance",
         "graph_name": "aws_db_instance.secret_manager",
         "description": "Not all patches and security fixes are automatically implemented in a database. "
-                       "The required work must be implemented during the maintenance window. "
-                       "https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/"
-                       "USER_UpgradeDBInstance.Maintenance.html#OS_Updates\n",
-        "remediation": 'Provide maintenance window based on your needs\n'
-                       'resource "aws_db_instance" "db_instance_name" '
-                       '{\n  ...\n  maintenance_window          = "Mon:00:00-Mon:03:00"\n}\n',
+        "The required work must be implemented during the maintenance window. "
+        "https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/"
+        "USER_UpgradeDBInstance.Maintenance.html#OS_Updates\n",
+        "remediation": "Provide maintenance window based on your needs\n"
+        'resource "aws_db_instance" "db_instance_name" '
+        '{\n  ...\n  maintenance_window          = "Mon:00:00-Mon:03:00"\n}\n',
         "issue": None,
-        "line_of_code": 77,
+        "line_of_code": 78,
     },
 ]
 
