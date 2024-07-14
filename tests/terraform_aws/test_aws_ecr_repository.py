dfb42371ec95c7625511cf53a7bfeff1e7fa7624
Mirek Zurek <zurek.miroslaw@gmail.com>
2024-07-14 18:27:34 +0200

    pinkhat-15 Added ECR policy to validate scan_on_push

--- tests/terraform_aws/test_aws_ecr_repository.py
+++ tests/terraform_aws/test_aws_ecr_repository.py
@@ -6,17 +6,39 @@ issues = [
     {
         "file_path": "tests/terraform_aws/test_files/aws_ecr_repository.tf",
         "category": "terraform",
+        "rule_name": "ECR Security scanning registry",
+        "module": "aws_ecr_repository",
+        "graph_name": "aws_ecr_repository.no_image_scanning",
+        "description": "The container image might have installed multiple different packages or libraries. "
+        "Those libraries might contain identified vulnerabilities i.e. CVEs. "
+        "By default, image scanning is manual and user initiated. "
+        "It adds unnecessary complexity to the process and might cause that your image won't be never "
+        "scanned. It is strongly recommended to enable a flag scan_on_push. "
+        "You can get more information in the link below: "
+        "https://docs.aws.amazon.com/AmazonECR/latest/userguide/image-scanning.html "
+        "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/"
+        "resources/ecr_repository#image_scanning_configuration "
+        "scan_on_push: if true, then image is scanned on the push, if false, then not scanned.\n",
+        "remediation": "Enable scan_on_push in your terraform code:\n"
+        'resource "aws_ecr_repository" "ecr_repository_name" {\n  ...\n  '
+        "image_scanning_configuration {\n    scan_on_push = true\n  }\n  ...\n}\n",
+        "issue": None,
+        "line_of_code": 10,
+    },
+    {
+        "file_path": "tests/terraform_aws/test_files/aws_ecr_repository.tf",
+        "category": "terraform",
         "rule_name": "Enable Deletion Protection",
         "module": "aws_ecr_repository",
         "graph_name": "aws_ecr_repository.force_delete",
         "description": "It's possible to delete ECR even if it contains images. "
-                       "The default option for aws_ecr_repository - force_delete is False "
-                       "force_delete: if true then deletes the repository with existing data in it\n",
-        "remediation": 'Disable force_delete option by changing it the value to false\n'
-                       'resource "aws_ecr_repository" "ecr_repository_name" {\n  ...\n  force_delete = false\n}\n',
+        "The default option for aws_ecr_repository - force_delete is False "
+        "force_delete: if true then deletes the repository with existing data in it\n",
+        "remediation": "Disable force_delete option by changing it the value to false\n"
+        'resource "aws_ecr_repository" "ecr_repository_name" {\n  ...\n  force_delete = false\n}\n',
         "issue": None,
         "line_of_code": 19,
-    }
+    },
 ]
 
 
