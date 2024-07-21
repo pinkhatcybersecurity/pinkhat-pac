0198fda5305b121b778e681bfaa632bc176b8372
Mirek Zurek <zurek.miroslaw@gmail.com>
2024-07-21 11:03:11 +0200

    pinkhat-33 fixed test

--- tests/terraform_aws/test_aws_vpc.py
+++ tests/terraform_aws/test_aws_vpc.py
@@ -9,12 +9,16 @@ issues = [
         "rule_name": "Missing flowlogs for VPC",
         "module": "aws_vpc",
         "graph_name": "aws_vpc.missing_flowlogs",
-        "description": "Flowlogs captures IP traffic for a specific VPC. Logs can be sent to S3, "
-        "CloudWatch or Amazon Kinesis\n",
-        "remediation": "",
-        "issue": "",
+        "description": "It's really important to get information about IP traffic in VPC. "
+        "If there is no enough logging information then it's impossible to track system communication "
+        "and spot if there is something wrong. It is really important in case of security incident. "
+        "Flowlogs captures IP traffic for a specific VPC. Logs can be sent to S3, CloudWatch or Amazon Kinesis\n",
+        "remediation": 'Consider adding Flowlogs to VPC:\nresource "aws_vpc" "flowlogs_enabled" '
+        '{\n  cidr_block = "127.0.0.1/16"\n}\nresource "aws_flow_log" "flowlog_exist" '
+        '{\n  traffic_type    = "ALL"\n  vpc_id          = aws_vpc.flowlogs_enabled.id\n}\n',
+        "issue": None,
         "line_of_code": 5,
-    },
+    }
 ]
 
 
