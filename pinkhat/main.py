dc56b0c1f565d8706a32639f66cf23c8a9aa19e3
Mirek Zurek <zurek.miroslaw@gmail.com>
2024-08-05 21:37:11 +0200

    #63 Added basic command line

--- pinkhat/main.py
+++ pinkhat/main.py
@@ -1,10 +1,47 @@
-from iacparsers.core_scanning import CoreScanning
+from argparse import ArgumentParser
+from pinkhat.iacparsers.core_scanning import CoreScanning
 
 
-def start(path: str):
+PINKHAT_VERSION = "1.0.0"
+
+
+def initialize_command_line() -> ArgumentParser:
+    parser = ArgumentParser(
+        prog="pinkhat",
+        usage="%(prog)s [OPTION] [DIRECTORY]...",
+        description=f"""
+        pinkhat {PINKHAT_VERSION}
+
+        Application executes defined policy checks on the given directory path
+        """,
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"{parser.prog} {PINKHAT_VERSION}",
+    )
+    parser.add_argument(
+        "files", nargs="*", help="file directory/directories or file/files", type=str
+    )
+    return parser
+
+
+def start_processing_data(paths: str | list[str]):
     core = CoreScanning()
-    core.start_scanning(path=path)
+    if str == type(paths):
+        paths = [paths]
+    for path in paths:
+        core.start_scanning(path=path)
     print(core.get_vulnerabilities_dict())
 
 
-start("/home/mirek/periculum/periculum-main/terraform")
+def main() -> None:
+    parser = initialize_command_line()
+    args = parser.parse_args()
+    if args.files:
+        start_processing_data(paths=args.files)
+
+
+if __name__ == "__main__":
+    main()
