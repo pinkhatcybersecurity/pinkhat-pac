3ea932403234dd6864914bd81e7bfdc313994a18
Mirek Zurek <zurek.miroslaw@gmail.com>
2024-08-24 10:00:03 +0200

    #67 Added missing changes

--- tests/terraform_aws/test_aws_elasticache_cluster.py
+++ tests/terraform_aws/test_aws_elasticache_cluster.py
@@ -8,52 +8,18 @@ issues = [
             "tests", "terraform_aws", "test_files", "aws_elasticache_cluster.tf"
         ),
         "category": "terraform",
-        "rule_name": "Maintenance Window for OS patching",
-        "module": "aws_elasticache_cluster",
-        "graph_name": "aws_elasticache_cluster.transit_enabled",
-        "description": "More information about maintenance window from AWS: "
-        "https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/"
-        "USER_UpgradeDBInstance.Maintenance.html#OS_Updates\n",
-        "remediation": "",
-        "issue": "",
-        "line_of_code": 1,
-    },
-    {
-        "file_path": os.path.join(
-            "tests", "terraform_aws", "test_files", "aws_elasticache_cluster.tf"
-        ),
-        "category": "terraform",
-        "rule_name": "Audit Log Management",
-        "module": "aws_elasticache_cluster",
-        "graph_name": "aws_elasticache_cluster.transit_enabled",
-        "description": "https://docs.aws.amazon.com/AmazonElastiCache/latest/red-ug/Log_Delivery.html",
-        "remediation": "",
-        "issue": None,
-        "line_of_code": 1,
-    },
-    {
-        "file_path": os.path.join(
-            "tests", "terraform_aws", "test_files", "aws_elasticache_cluster.tf"
-        ),
-        "category": "terraform",
         "rule_name": "Encrypt Sensitive Data at Transit",
         "module": "aws_elasticache_cluster",
         "graph_name": "aws_elasticache_cluster.no_encryption_in_transit",
-        "description": "https://docs.aws.amazon.com/AmazonElastiCache/latest/mem-ug/in-transit-encryption.html\n",
-        "remediation": "",
-        "issue": "",
-        "line_of_code": 11,
-    },
-    {
-        "file_path": os.path.join(
-            "tests", "terraform_aws", "test_files", "aws_elasticache_cluster.tf"
-        ),
-        "category": "terraform",
-        "rule_name": "Audit Log Management",
-        "module": "aws_elasticache_cluster",
-        "graph_name": "aws_elasticache_cluster.no_encryption_in_transit",
-        "description": "https://docs.aws.amazon.com/AmazonElastiCache/latest/red-ug/Log_Delivery.html",
-        "remediation": "",
+        "description": "Data send over the internet must be encrypted to protect information from "
+        "unauthorized disclosure or modifications. End to end encryption should be used. "
+        "TLS is the common cryptographic protocol.\n"
+        "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticache_cluster#"
+        "transit_encryption_enabled\n"
+        "https://docs.aws.amazon.com/AmazonElastiCache/latest/mem-ug/in-transit-encryption.html\n",
+        "remediation": "Enable transit_encryption_enabled in aws_elasticache_cluster.\n"
+        'resource "aws_elasticache_cluster" "elasticache_cluster" {\n  ...\n'
+        "  transit_encryption_enabled = true\n  ...\n}\n",
         "issue": None,
         "line_of_code": 11,
     },
@@ -65,24 +31,16 @@ issues = [
         "rule_name": "Encrypt Sensitive Data at Transit",
         "module": "aws_elasticache_cluster",
         "graph_name": "aws_elasticache_cluster.contains_both_log_delivery_configuration",
-        "description": "https://docs.aws.amazon.com/AmazonElastiCache/latest/mem-ug/in-transit-encryption.html\n",
-        "remediation": "",
-        "issue": "",
-        "line_of_code": 21,
-    },
-    {
-        "file_path": os.path.join(
-            "tests", "terraform_aws", "test_files", "aws_elasticache_cluster.tf"
-        ),
-        "category": "terraform",
-        "rule_name": "Maintenance Window for OS patching",
-        "module": "aws_elasticache_cluster",
-        "graph_name": "aws_elasticache_cluster.contains_both_log_delivery_configuration",
-        "description": "More information about maintenance window from AWS: "
-        "https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/"
-        "USER_UpgradeDBInstance.Maintenance.html#OS_Updates\n",
-        "remediation": "",
-        "issue": "",
+        "description": "Data send over the internet must be encrypted to protect information from "
+        "unauthorized disclosure or modifications. End to end encryption should be used. "
+        "TLS is the common cryptographic protocol.\n"
+        "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticache_cluster#"
+        "transit_encryption_enabled\n"
+        "https://docs.aws.amazon.com/AmazonElastiCache/latest/mem-ug/in-transit-encryption.html\n",
+        "remediation": "Enable transit_encryption_enabled in aws_elasticache_cluster.\n"
+        'resource "aws_elasticache_cluster" "elasticache_cluster" {\n  ...\n'
+        "  transit_encryption_enabled = true\n  ...\n}\n",
+        "issue": None,
         "line_of_code": 21,
     },
     {
@@ -93,24 +51,16 @@ issues = [
         "rule_name": "Encrypt Sensitive Data at Transit",
         "module": "aws_elasticache_cluster",
         "graph_name": "aws_elasticache_cluster.onlyfirehose",
-        "description": "https://docs.aws.amazon.com/AmazonElastiCache/latest/mem-ug/in-transit-encryption.html\n",
-        "remediation": "",
-        "issue": "",
-        "line_of_code": 42,
-    },
-    {
-        "file_path": os.path.join(
-            "tests", "terraform_aws", "test_files", "aws_elasticache_cluster.tf"
-        ),
-        "category": "terraform",
-        "rule_name": "Maintenance Window for OS patching",
-        "module": "aws_elasticache_cluster",
-        "graph_name": "aws_elasticache_cluster.onlyfirehose",
-        "description": "More information about maintenance window from AWS: "
-        "https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/"
-        "USER_UpgradeDBInstance.Maintenance.html#OS_Updates\n",
-        "remediation": "",
-        "issue": "",
+        "description": "Data send over the internet must be encrypted to protect information from "
+        "unauthorized disclosure or modifications. End to end encryption should be used. "
+        "TLS is the common cryptographic protocol.\n"
+        "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticache_cluster#"
+        "transit_encryption_enabled\n"
+        "https://docs.aws.amazon.com/AmazonElastiCache/latest/mem-ug/in-transit-encryption.html\n",
+        "remediation": "Enable transit_encryption_enabled in aws_elasticache_cluster.\n"
+        'resource "aws_elasticache_cluster" "elasticache_cluster" {\n  ...\n'
+        "  transit_encryption_enabled = true\n  ...\n}\n",
+        "issue": None,
         "line_of_code": 42,
     },
 ]
